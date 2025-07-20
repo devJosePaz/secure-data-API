eaf3726b52a7c27f5305a8a0a390c783c184e165
devJosePaz <lusjos40@gmail.com>
2025-07-20 12:01:40 -0300

    insercao de tratativa de erros na rota POST

--- app/routes/transaction_routes.py
+++ app/routes/transaction_routes.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, Request, HTTPException
 from sqlalchemy.orm import Session
 import schemas, models
 from databse import get_db
 
-router = APIRouter(prefix="/transaction")
+router = APIRouter(prefix="/transaction", tags=["transactions"])
 
 @router.get("/transactions/", response_model=list[schemas.TransactionResponse])
 def list_transactions(db: Session = Depends(get_db)):
@@ -13,13 +13,20 @@ def list_transactions(db: Session = Depends(get_db)):
 
 @router.post("/transactions/", response_model=schemas.TransactionResponse)
 def create_transaction(transaction: schemas.TransactionCreate, db: Session = Depends(get_db)):
-    new_transaction = models.Transaction(
-        user_document=transaction.user_document,
-        credit_card_token=transaction.credit_card_token,
-        value=transaction.value
-    )
-    db.add(new_transaction)
-    db.commit()
-    db.refresh(new_transaction)
+    if transaction.value <= 0:
+        raise HTTPException(status_code=400, detail="error the value must be greater than or equal to 0")
 
-    return new_transaction
+    new_transaction = Transaction(
+            user_document=transaction.user_document,
+            credit_card_token=transaction.credit_card_token,
+            value=transaction.value
+        )
+
+    try:
+        db.add(new_transaction)
+        db.commit()
+        db.refresh(new_transaction)
+        return  new_transaction
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="error creating transaction")
